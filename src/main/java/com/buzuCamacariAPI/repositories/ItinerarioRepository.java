232f9b5e31818c192bdd75312f8594ab7dc1d29d
Jonas Magalhães Silva <80786958+jonasmagalhaes-8@users.noreply.github.com>
2023-05-22 01:37:32 -0300

    Adiciona ordenação crescente por Itinerario

--- src/main/java/com/buzuCamacariAPI/repositories/ItinerarioRepository.java
+++ src/main/java/com/buzuCamacariAPI/repositories/ItinerarioRepository.java
@@ -1,10 +1,13 @@
 package com.buzuCamacariAPI.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.buzuCamacariAPI.models.ItinerarioModel;
 
 @Repository
 public interface ItinerarioRepository extends JpaRepository<ItinerarioModel, Integer> {
-	
+	public List<ItinerarioModel> findAllByOrderByItinerarioAsc();
 }
